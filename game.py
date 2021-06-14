e1c28836bfef944921d6e38224fd4a6075b4509d
Shishir8957 <shishir.bastola8957@gmail.com>
2021-06-14 11:31:27 +0545

    update

--- game.py
+++ game.py
@@ -4,8 +4,8 @@ import time
 # parmanent variables
 WIDTH = 500
 HEIGHT = 500
-dx = 1
-dy = 1
+dx = 2
+dy = 2
 
 
 #setting up working space 
@@ -56,7 +56,16 @@ def left():
     bat.setx(x-20)
 def right():
     x = bat.xcor()
-    bat.setx(x+20)    
+    bat.setx(x+1)
+     
+# def move():
+#     if a == 1:
+#         x=bat.xcor()
+#         bat.setx(x-1)
+#     else:
+#         x = bat.xcor()
+#         bat.setx(x+1)
+
 
 #ball movement 
 def movement():
@@ -66,7 +75,7 @@ def movement():
 def game_over():
     window.clear()
     game = turtle.Turtle()
-    game.hideturtle()#
+    game.hideturtle()
     game.write('Game Over', font=('arial', 50), align='center')
     time.sleep(3)
 
@@ -79,6 +88,8 @@ window.onkeypress(right,'Right')
 
 while True:
     movement()
+    # move()
+
 
     #ball return when strick at border 
     if ball.xcor() >= 250:
@@ -88,13 +99,19 @@ while True:
     if ball.ycor() >= 250:
         dy*=-1
 
+    #bat stop when it     
+    if bat.xcor() <= -190:
+        bat.setx(-190)
+    if bat.xcor() >= 190:
+        bat.setx(190)   
+
     #game over when ball misses to hit ball    
     if ball.ycor() <= -250:
         game_over()
 
     #when bat hits the ball    
-    if (ball.ycor() <= -220) and (ball.xcor()>bat.xcor()-50 and ball.xcor()<bat.xcor()+50):
+    if (ball.ycor() <= -220) and (ball.xcor()>bat.xcor()-60 and ball.xcor()<bat.xcor()+60):
         ball.sety(-220)
         dy *= -1 
-
+              
     window.update()
